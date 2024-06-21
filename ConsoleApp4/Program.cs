38c31f39365df99b494380f20bfb843bffdc5ec4
Name <email@example.ru>
2024-06-21 15:59:56 +0300

    (HW-03)(3)

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace ConsoleApp4
+﻿using System;
+using System.IO;
+
+public class FolderSizeCalculator
 {
-    internal class Program
+    public static long CalculateFolderSize(string directoryPath)
     {
-        static void Main(string[] args)
+        long totalSize = 0;
+
+        
+        if (!Directory.Exists(directoryPath))
         {
-            Console.WriteLine("Hello, World!");
+            throw new ArgumentException("Папка не существует.");
+        }
+
+        
+        foreach (string filePath in Directory.EnumerateFiles(directoryPath, "*", SearchOption.AllDirectories))
+        {
+            FileInfo fileInfo = new FileInfo(filePath);
+            totalSize += fileInfo.Length;
+        }
+
+        return totalSize;
+    }
+
+    public static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Необходимо указать путь к папке в качестве аргумента.");
+            return;
+        }
+
+        string folderPath = args[0];
+
+        try
+        {
+            long sizeInBytes = CalculateFolderSize(folderPath);
+            Console.WriteLine($"Размер папки '{folderPath}': {sizeInBytes} байт");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
         }
     }
 }
+
